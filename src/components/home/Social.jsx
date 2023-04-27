69e8cf7e314600cf5a37c4e4aea40e418279a1a8
MuMu0502 <phanchau0502@gmail.com>
2023-04-28 00:34:50 +0700

    thêm header va home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.facebook.com/phanchau0502" className="home__social-icon" target="_blank">
+        <i class="uil uil-facebook"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/phan-chau52/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/MuMu0502" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
